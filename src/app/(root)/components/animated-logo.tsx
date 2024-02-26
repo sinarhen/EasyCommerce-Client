9e627e0cb89e9d91a04f59031c183439a7efd2a7
sinarhen <nicklchannel8@gmail.com>
2024-02-26 21:45:52 +0200

    navbar.tsx added animated  gradient text to logo

--- src/app/(root)/components/animated-logo.tsx
+++ src/app/(root)/components/animated-logo.tsx
@@ -1,33 +1,56 @@
-'use client'
+'use client';
+
 
 import { motion } from "framer-motion";
 import { Store } from "lucide-react";
 import React from "react";
 
-
 const logoVariants = {
-  hidden: { opacity: 0, x: -20},
+  hidden: { opacity: 0, x: -20 },
   visible: { opacity: [0.05, 0.3, 0.5, 0.7, 1], x: 0 },
+};
 
-}
+const textVariants = {
+  hidden: { x: -50, opacity:0 },
+  visible: { x: 0, opacity: 1 },
+};
+
+const transitionDuration = 1;
 
 const logoTransition = {
-  duration: 1,
+  duration: transitionDuration,
   animation: "easeInOut",
   ease: "easeOut",
-}
+};
+
+const textTransition = {
+  delay: transitionDuration - 0.2,
+  duration: 0.5,
+  type: "spring",
+  stiffness: 120,
+};
 
 export default function AnimatedLogo() {
   return (
-    <motion.div
-      initial="hidden"
-      animate="visible"
-      variants={logoVariants}
-      transition={logoTransition}
-      className="flex items-center hover:text-gray-400 transition-colors justify-center w-12 h-12 bg-zinc-900 rounded-full"
-    >
-
-      <Store size={32} />
-    </motion.div>
+    <span className="flex items-center ">
+      <motion.div
+        initial="hidden"
+        animate="visible"
+        variants={logoVariants}
+        transition={logoTransition}
+        className="flex items-center hover:text-gray-400 transition-colors justify-center w-12 h-12 bg-zinc-900 rounded-full"
+      >
+        <Store size={24} />
+      </motion.div>
+      <motion.div
+        className="text-gradient transition-all text-sm font-bold animate-gradient"
+        initial="hidden"
+        animate="visible"
+        variants={textVariants}
+        transition={textTransition}
+      >
+        EasyComm
+      </motion.div>
+    </span>
   );
 }
