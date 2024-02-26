439e5d173b4a1b19f4be731ff6acfdd8f34e6363
sinarhen <nicklchannel8@gmail.com>
2024-02-26 20:24:23 +0200

    Themes configuration and (root) folder added

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import {Button} from "@/components/ui/button";
+
+export default function Home() {
+
+  return (
+    <main className="transition-colors duration-500 dark:bg-zinc-900 bg-gray-100 min-h-screen">
+
+
+    </main>
+  );
+}
