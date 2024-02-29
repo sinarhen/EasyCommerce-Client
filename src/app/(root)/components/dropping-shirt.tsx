96594fb044b1234b6c2862eca690461a48ff600a
sinarhen <nicklchannel8@gmail.com>
2024-02-29 18:20:47 +0200

    dropping shirt responsiveness fixes

--- src/app/(root)/components/dropping-shirt.tsx
+++ src/app/(root)/components/dropping-shirt.tsx
@@ -9,7 +9,7 @@ const DroppingShirt = ({initialX, delay, shirtDropDuration, price}: {
 }) => {
   return (
     <motion.div
-      className={"absolute flex items-center justify-center w-fit inset-0 mx-auto h-20 md:h-28 lg:h-28 xl:h-32 -top-24 md:-top-36  xl:-top-36"}
+      className={"absolute flex items-center justify-center w-fit inset-0 mx-auto h-20 sm:h-24 md:h-28 lg:h-28 xl:h-32 -top-24 sm:-top-[7.5rem] md:-top-36  xl:-top-36"}
       initial={{x: initialX, y: "0%", opacity: 0, }}
       animate={{ y: ["0%", "100%", "0%"], x:[initialX, "0px", initialX], scale: [1, 0.5, 1], opacity: [0, 1, 0.05, 0, 0] }}
       transition={{ repeat: Infinity, duration: shirtDropDuration, delay: delay, ease: "easeInOut"}}
