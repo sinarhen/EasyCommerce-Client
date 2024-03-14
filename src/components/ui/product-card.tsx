cd7b2823a8a135be26daa11c64fdf554f1bda11b
sinarhen <nicklchannel8@gmail.com>
2024-03-14 17:47:02 +0200

    Styling of product-card changed

--- src/components/ui/product-card.tsx
+++ src/components/ui/product-card.tsx
@@ -1,12 +1,13 @@
 'use client';
 
 
-import {ProductDto} from "@/types/product";
+import {ColorDto, ProductDto} from "@/types/product";
 import {Button} from "@/components/ui/button";
-import {Backpack, PersonStanding, ShoppingCart, Star, Sun} from "lucide-react";
+import {Backpack, PersonStanding, Shirt, ShoppingCart, Star, Sun} from "lucide-react";
 import {iconSizes} from "@/lib/constants";
 import Image from "next/image";
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import {Tooltip, TooltipContent, TooltipTrigger} from "@/components/ui/tooltip";
 
 export default function ProductCard({
   product,
@@ -14,30 +15,24 @@ export default function ProductCard({
   product: ProductDto
 }) {
   const [selectedImage, setSelectedImage] = useState(product.images[0].imageUrls[0])
-  console.log(product.colors)
+  const [imageIsLoading, setImageIsLoading] = useState(true);
+  const [selectedColor, setSelectedColor] = useState<ColorDto | undefined>(product.colors[0]);
+
   return (
     <div className="group relative">
-      <div className="bg-white group-hover:absolute w-full flex-col flex h-full rounded-md drop-shadow-lg p-4">
+      <div className="bg-white group-hover:absolute w-full flex-col flex h-full rounded-sm drop-shadow-lg ">
         {product.isNew && (
           <div className="absolute -top-3 -left-4 px-2 py-1 z-20 bg-purple-800 rounded bg-opacity-90 text-sm">New</div>
         )}
 
 
-        <div className="relative min-h-[300px] overflow-hidden w-full bg-gray-300 rounded">
-          <div className="absolute top-3 right-1 flex w-full items-end justify-end gap-x-2 z-40  transition-opacity rounded hover:bg-opacity-100 ">
-            {product.colors.map((color, index) => (
-              <div key={index} className="w-4 h-4 rounded-full" style={{backgroundColor: color.hexCode}}/>
-            ))}
-          </div>
-          <div
-            className="absolute flex sm:text-xs text-lg   rounded-2xl text-gray-400 font-semibold items-center gap-x-1 top-2 left-2 sm:left-1">
-            <Star size={iconSizes.md} className="h-full w-full"/>
-            4.5
+        <div className="relative min-h-[300px] overflow-hidden w-full bg-gray-300 ">
 
-          </div>
           <Image
-            className="object-cover hover:scale-110 transition-transform"
+            className={`${imageIsLoading ? "animate-pulse bg-gray-200 blur-md" : ""} transition-all object-cover hover:scale-110 `}
             quality={80}
+            loading="lazy"
+            onLoad={() => setImageIsLoading(false)}
             src={selectedImage}
             fill
             alt={"None"}/>
@@ -55,7 +50,7 @@ export default function ProductCard({
             ))}
           </div>
         </div>
-        <div className="flex mt-3 items-center w-full justify-between">
+        <div className="flex mt-3 px-4 items-center w-full justify-between">
           <h3 className="font-light text-2xl sm:text-lg line-clamp-1 text-black mt-1">{product.name}</h3>
           <div className="flex gap-x-1 text-xs items-center h-full text-gray-400 ">
             {product.categories.reverse().map((category, index) => (
@@ -65,7 +60,7 @@ export default function ProductCard({
           </div>
         </div>
         <hr className="h-px my-1"/>
-        <div className="justify-self-end flex justify-between items-center">
+        <div className="justify-self-end px-4 flex justify-between items-center">
           <span className=" text-gradient animate-gradient  text-lg ">${product.minPrice}</span>
           <div>
             <Button size={"sm"} variant="outline" className='mt-2 group'>
@@ -80,17 +75,65 @@ export default function ProductCard({
           </div>
         </div>
 
-        {/*<div className="w-full absolute mt-3 bg-white justify-between flex items-center font-semibold text-xs">*/}
-        {/*<span*/}
-        {/*  className="text-orange-300 opacity-40 hover:opacity-100 transition-opacity cursor-pointer items-center flex gap-x-1.5"><Sun*/}
-        {/*  size={iconSizes.sm}/> {product.season}</span>*/}
-        {/*  <span*/}
-        {/*    className="text-black flex opacity-40 hover:opacity-100 transition-opacity cursor-pointer items-center gap-x-1"><Backpack*/}
-        {/*    size={iconSizes.sm}/> {product.occasion.name}</span>*/}
-        {/*  <span*/}
-        {/*    className="text-blue-800 flex items-center opacity-40 hover:opacity-100 transition-opacity cursor-pointer gap-x-1"><PersonStanding*/}
-        {/*    size={iconSizes.sm}/> {product.gender}</span>*/}
-        {/*</div>*/}
+        <div className="px-4 w-full flex justify-start">
+
+          <div
+            className="flex w-1/2  gap-x-1 ">
+            {product.colors.length > 0 ? product.colors.map((color, index) => (
+              <Tooltip key={color.id}>
+                <TooltipTrigger>
+                  <div
+                    onClick={() => setSelectedColor(color)}
+                    className="w-6 border border-gray-300 opacity-90 hover:opacity-100 hover:scale-125 transition-all h-6 cursor-pointer rounded-full"
+                    style={{backgroundColor: color.hexCode}}/>
+
+                </TooltipTrigger>
+                <TooltipContent>
+                  {color.name.normalize()}
+                </TooltipContent>
+              </Tooltip>
+            )): (
+              <span className="h-6 flex text-sm text-center w-full italic items-center">No colors</span>
+            )}
+          </div>
+        </div>
+
+        <div className="w-full pb-4 px-4 mt-3 bg-white justify-between flex flex-col font-semibold text-xs">
+
+          <span
+            className=" opacity-40 hover:opacity-100 w-full transition-opacity cursor-pointer items-center flex justify-between ">
+            <div className="flex gap-x-1 items-center">
+              <Sun
+                size={iconSizes.sm}/>
+              Season
+            </div>
+            {product.season}
+
+          </span>
+
+          <span
+            className=" opacity-40 hover:opacity-100 w-full transition-opacity cursor-pointer items-center flex justify-between ">
+            <div className="flex gap-x-1 items-center">
+            <Shirt
+              size={iconSizes.sm}/>
+              Occasion
+            </div>
+            {product.occasion.name}
+
+          </span>
+
+          <span
+            className=" opacity-40 hover:opacity-100 w-full transition-opacity cursor-pointer items-center flex justify-between ">
+            <div className="flex gap-x-1 items-center">
+            <PersonStanding
+              size={iconSizes.sm}
+            />
+              Gender
+            </div>
+            {product.gender}
+
+          </span>
+        </div>
 
       </div>
 
