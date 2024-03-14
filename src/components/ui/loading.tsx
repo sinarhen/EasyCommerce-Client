26be5e9a683b5d5f659aca0a6f98e9f2fd44f68f
sinarhen <nicklchannel8@gmail.com>
2024-03-14 23:22:29 +0200

    Working on loading

---
+++ src/components/ui/loading.tsx
@@ -0,0 +1,19 @@
+import {cn} from "@/lib/utils";
+
+const sizes = {
+  "xs": "w-4 h-4",
+  "sm": "w-6 h-6",
+  "md": "w-8 h-8",
+  "lg": "w-10 h-10",
+  "xl": "w-12 h-12",
+
+}
+export default function Loading() {
+  return (
+    <div
+      className="w-6 h-6 rounded-full animate-spin border border-dashed border-white border-t-transparent">
+
+    </div>
+
+  )
+}
