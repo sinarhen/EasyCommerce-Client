367cc48925f547cf0e650a206bb9345456c4d4fe
sinarhen <nicklchannel8@gmail.com>
2024-03-15 22:55:21 +0200

    Filters UI is ready

--- src/app/store/components/filters.tsx
+++ src/app/store/components/filters.tsx
@@ -11,8 +11,72 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet"
-import {Filter} from "lucide-react";
+import {ArrowDown, ChevronDown, Filter} from "lucide-react";
 import {iconSizes} from "@/lib/constants";
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "@/components/ui/collapsible";
+import {Checkbox} from "@/components/ui/checkbox";
+import React from "react";
+
+
+export function FilterSectionGroupCheckbox({
+  title,
+  id,
+                                           }: {
+  title: string,
+  id: string
+}){
+  return (
+    <div className="flex items-center gap-x-1">
+      <Checkbox id={id}/>
+      <Label htmlFor={id}>
+        {title}
+      </Label>
+    </div>
+  )
+}
+
+export function FilterSectionGroup({
+                                     children
+                                   }: {
+  children: React.ReactNode
+}) {
+  return (
+    <div className="mt-2 gap-y-2 flex flex-col">
+      {children}
+    </div>
+  )
+}
+
+export function FilterSection({
+  title,
+  description,
+  children
+                                      }: {
+  title: string,
+  description: string
+  children: React.ReactNode
+}) {
+  return (
+    <Collapsible>
+      <CollapsibleTrigger>
+        <h4 className=" flex items-center font-semibold">
+          {title}
+          <ChevronDown size={iconSizes.md} />
+        </h4>
+
+      </CollapsibleTrigger>
+      <CollapsibleContent>
+        <p className="text-sm text-gray-500">
+          {description}
+        </p>
+
+        {children}
+
+      </CollapsibleContent>
+    </Collapsible>
+
+  )
+}
 
 export function Filters() {
   return (
@@ -30,14 +94,49 @@ export function Filters() {
             Filters for products
           </SheetDescription>
         </SheetHeader>
-        <Label htmlFor="searchTerm">
-          Search
-        </Label>
-        <Input type="text" id="searchTerm" placeholder="Search for products"/>
+        <div className="flex flex-col gap-y-2">
+          <div>
+            <Label htmlFor="searchTerm">
+              Search
+            </Label>
+            <Input type="text" id="searchTerm" placeholder="Search for products"/>
+
+          </div>
+          <FilterSection title={"Category"} description={"Filter by category"}>
+            <FilterSectionGroup>
+              <FilterSectionGroupCheckbox title={"Shirts"} id={"shirts"}/>
+              <FilterSectionGroupCheckbox title={"Pants"} id={"pants"}/>
+              <FilterSectionGroupCheckbox title={"Shoes"} id={"shoes"}/>
+              <FilterSectionGroupCheckbox title={"Accessories"} id={"accessories"}/>
+            </FilterSectionGroup>
+          </FilterSection>
+          <FilterSection title={"Color"} description={"Filter by color"}>
+            <FilterSectionGroup>
+              <FilterSectionGroupCheckbox title={"Red"} id={"red"}/>
+              <FilterSectionGroupCheckbox title={"Blue"} id={"blue"}/>
+              <FilterSectionGroupCheckbox title={"Green"} id={"green"}/>
+              <FilterSectionGroupCheckbox title={"Yellow"} id={"yellow"}/>
+            </FilterSectionGroup>
+          </FilterSection>
+          <FilterSection title={"Size"} description={"Filter by size"}>
+            <FilterSectionGroup>
+              <FilterSectionGroupCheckbox title={"Small"} id={"small"}/>
+              <FilterSectionGroupCheckbox title={"Medium"} id={"medium"}/>
+              <FilterSectionGroupCheckbox title={"Large"} id={"large"}/>
+              <FilterSectionGroupCheckbox title={"Extra Large"} id={"xl"}/>
+            </FilterSectionGroup>
+          </FilterSection>
+          <FilterSection title={"Price"} description={"Filter by price"}>
+            <div className="flex items-center gap-x-1">
+              <Input type="number" id="minPrice" placeholder="Min"/>
+              <Input type="number" id="maxPrice" placeholder="Max"/>
+            </div>
+          </FilterSection>
+        </div>
 
         <SheetFooter>
           <SheetClose asChild>
-            <Button className="mt-2" type="submit">Apply</Button>
+            <Button className="mt-2 w-full" type="submit">Apply</Button>
           </SheetClose>
         </SheetFooter>
       </SheetContent>
