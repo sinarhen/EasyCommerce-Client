138e5d8111180b198b7228bdb6ac7868fd68aaf3
sinarhen <nicklchannel8@gmail.com>
2024-03-16 18:25:49 +0200

    SMall improvements

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -4,6 +4,9 @@ import AnimatedProducts from "@/app/store/components/animated-products";
 import { Filters } from "./components/filters";
 import Image from "next/image";
 import {AspectRatio} from "@/components/ui/aspect-ratio";
+import {Eye, Star} from "lucide-react";
+import {iconSizes} from "@/lib/constants";
+import { Button } from "@/components/ui/button";
 
 
 export async function getData() {
@@ -19,9 +22,21 @@ export default async function Store() {
     <div className='w-full '>
       <div className="flex overflow-y-hidden mb-5 overflow-x-auto flex-wrap gap-x-2">
         <div className="group  overflow-hidden cursor-pointer relative bg-gray-300 rounded w-[200px] h-[200px]">
-          <div className="group-hover:bg-black/90 transition-all px-4 py-5 bg-black/70 absolute z-20 w-full h-full">
-            <h1 className="text-2xl font-bold text-gradient animate-gradient">Shirts</h1>
-            <p className="text-white">Check out Shirts category</p>
+          <div className=" flex-col justify-between flex group-hover:bg-black/90 transition-all px-4 py-5 bg-black/70 absolute z-20 w-full h-full">
+            <div>
+              <h1 className="text-2xl font-bold text-gradient animate-gradient">Shirts</h1>
+              <p className="text-white">Check out Shirts category</p>
+
+            </div>
+            <div className="flex justify-between w-full">
+              <Button size={"sm"} variant="outline" className='group-hover:translate-y-0 group/wish  transition-all opacity-0 group-hover:opacity-100 translate-y-3'>
+                <div className="group-hover/wish:translate-x-0.5 transition-transform flex gap-x-2 ease-out ">
+                  <Eye size={iconSizes.md}></Eye>
+                  View
+                </div>
+              </Button>
+            </div>
+
           </div>
           <AspectRatio ratio={1}>
             <Image src={products[0]?.images[0]?.imageUrls[0]} fill alt="Image"
