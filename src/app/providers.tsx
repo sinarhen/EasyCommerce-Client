9a6bfafcc29e8498bb66a34fe8a273ce411ff7b2
sinarhen <nicklchannel8@gmail.com>
2024-03-17 13:05:14 +0200

    React query initialized in providers

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,27 +1,37 @@
 "use client";
 
-import React, {ReactNode} from "react";
+import React, {ReactNode, useState} from "react";
 import {ThemeProvider} from "next-themes";
 import {TooltipProvider} from "@/components/ui/tooltip";
 import {Toaster} from "react-hot-toast";
 import {AuthProvider} from "@/contexts/AuthContext";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
 
-const Providers = ({children}: { children: ReactNode }) => (
-  <ThemeProvider
-    defaultTheme="light"
-    attribute="class"
+const Providers = ({children}: { children: ReactNode }) => {
+  const [queryClient] = useState(() => new QueryClient());
 
-    storageKey="theme"
-  >
-    <TooltipProvider>
-      <Toaster/>
-      <AuthProvider>
-        {children}
-      </AuthProvider>
-    </TooltipProvider>
-  </ThemeProvider>
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <ThemeProvider
+        defaultTheme="light"
+        attribute="class"
 
-);
+        storageKey="theme"
+      >
+        <TooltipProvider>
+          <Toaster/>
+          <AuthProvider>
+            {children}
+          </AuthProvider>
+        </TooltipProvider>
+      </ThemeProvider>
+    </QueryClientProvider>
+
+  )
+};
+
 
 
 export default Providers;
