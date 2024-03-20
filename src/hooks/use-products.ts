012a2854b4047130c8afd4dc9d90d563328c4d4f
sinarhen <nicklchannel8@gmail.com>
2024-03-20 14:17:36 +0200

    Optional initial requests

--- src/hooks/use-products.ts
+++ src/hooks/use-products.ts
@@ -2,7 +2,7 @@ import {ProductDto} from "@/types/product";
 import {useQuery, UseQueryResult} from "@tanstack/react-query";
 import {getProducts} from "@/actions/products";
 
-export default function useProducts(products: ProductDto[]): UseQueryResult<ProductDto[]>{
+export default function useProducts(products?: ProductDto[]): UseQueryResult<ProductDto[]>{
   return useQuery({
     queryKey: ["products"],
     queryFn: async () => {
