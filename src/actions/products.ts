d4cfc0d3b8fb3970d0aedf6937826478c6aeb5ce
sinarhen <nicklchannel8@gmail.com>
2024-03-22 19:21:23 +0200

    Working on product details page design

--- src/actions/products.ts
+++ src/actions/products.ts
@@ -1,14 +1,14 @@
 'use server'
 
 import apiFetcher from "@/actions/api";
-import {ProductsResponse, ProductsSearchParams} from "@/types/product";
+import {ProductDetailsDto, ProductsResponse, ProductsSearchParams} from "@/types/product";
 
 export async function getProducts(params?: string): ProductsResponse {
   const products = await apiFetcher.get("/products" + (params ? ("?" + params) : ""))
   return products.data
 }
 
-export async function getProduct(id: string) {
+export async function getProduct(id: string): ProductDetailsDto {
   const product = await apiFetcher.get("/products/" + id)
   return product.data
 }
--- src/app/store/products/[productId]/page.tsx
+++ src/app/store/products/[productId]/page.tsx
@@ -1,7 +1,8 @@
-
-
 import {getProduct} from "@/actions/products";
-import AnimatedCategories from "@/app/store/components/animated-categories";
+import {AspectRatio} from "@/components/ui/aspect-ratio";
+import React from "react";
+import Image from "next/image";
+import Header from "@/components/ui/header";
 
 export default async function ProductDetailsPage({
   params
@@ -11,11 +12,63 @@ export default async function ProductDetailsPage({
   }
 }){
   const product = await getProduct(params.productId);
-  console.log(product)
   return (
-    <div className="w-full">
-      {product.name}
+    <div className="w-full grid gap-x-3 gap-y-2 grid-cols-1 md:grid-cols-2">
+      <div className="gap-y-1.5 flex flex-col">
+        <div className="w-full rounded p-[0.05rem] animate-gradient bg-gradient ">
+          <AspectRatio ratio={1}>
+            <Image
+              className='rounded object-cover w-full h-full'
+              fill
+              src={product.images[0].imageUrls[0]}
+              alt={"No image"}
 
+            />
+          </AspectRatio>
+
+        </div>
+        <div className="flex gap-x-1">
+          {product.images.map((image, index) => (
+            <div key={index} className="w-1/4 rounded p-[0.05rem] animate-gradient bg-gradient">
+              <AspectRatio ratio={1}>
+                <Image
+                  className='rounded object-cover w-full h-full'
+                  fill
+                  src={image.imageUrls[0]}
+                  alt={"No image"}
+
+                />
+              </AspectRatio>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="w-full">
+        <Header>
+          {product.name.split(" ").slice(0, -1).join(" ") + " "}
+
+          <span className="animate-gradient text-gradient">
+              {product.name.split(" ").slice(-1)}
+            </span>
+        </Header>
+        <span className='mt-3'>
+          Color:
+          <span className='font-bold'>
+            {" " + product.colors[0].name}
+
+          </span>
+        </span>
+        <div className="flex gap-x-1 mt-1">
+
+          {product.colors.map((color, index) => (
+            <button key={index} className="w-8 h-8 rounded-full" style={{backgroundColor: color.hexCode}}></button>
+          ))}
+        </div>
+        <hr className="h-px my-3 bg-gray-200 opacity-90 rounded-full bg-gradient animate-gradient border-0 "/>
+        <p>{product.description}</p>
+        <p>{product.price}</p>
+        <button>Add to cart</button>
+      </div>
     </div>
   )
 
