3102fe6e782ed9fc50f74c33a6bcb54ac16d3731
sinarhen <nicklchannel8@gmail.com>
2024-03-23 10:23:50 +0200

    Labels on products-pagination.tsx and products-page-size-selector.tsx

--- src/app/store/components/products-page-size-selector.tsx
+++ src/app/store/components/products-page-size-selector.tsx
@@ -13,6 +13,7 @@ import {useParamsStore} from "@/hooks/use-params-store";
 import React, {useCallback} from "react";
 import {shallow} from "zustand/shallow";
 import {productsPageSizes} from "@/lib/constants";
+import {Label} from "@/components/ui/label";
 
 export default function ProductsPageSizeSelector() {
   const params = useParamsStore((state) => {
@@ -29,24 +30,30 @@ export default function ProductsPageSizeSelector() {
     })
   }, [params])
   return (
-    <Select onValueChange={value => {
-      changePageSize(Number(value))
-    }}>
-      <SelectTrigger className="mt-4 w-[180px]">
-        <SelectValue placeholder={params.pageSize ?? "Select page size"}/>
-      </SelectTrigger>
-      <SelectContent>
-        <SelectGroup>
-          {productsPageSizes.map(size => (
-            <SelectItem
-              key={size}
-              value={size}
-            >
-              <SelectLabel>{size}</SelectLabel>
-            </SelectItem>
-          ))}
-        </SelectGroup>
-      </SelectContent>
-    </Select>
-  )
+    <div className="flex flex-col">
+      <Label className="mt-3 mb-1">Page size</Label>
+      <Select onValueChange={value => {
+        changePageSize(Number(value))
+      }}>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder={params.pageSize ?? "Select page size"}/>
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectLabel className="text-sm">Page size</SelectLabel>
+
+            {productsPageSizes.map(size => (
+              <SelectItem
+                key={size}
+                value={size}
+              >
+                <SelectLabel>{size}</SelectLabel>
+              </SelectItem>
+            ))}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+
+    </div>
+    )
 }
--- src/app/store/components/products-pagination.tsx
+++ src/app/store/components/products-pagination.tsx
@@ -9,9 +9,10 @@ import {
   PaginationPrevious,
 } from "@/components/ui/pagination"
 import {useParamsStore} from "@/hooks/use-params-store";
-import {useCallback} from "react";
+import React, {useCallback} from "react";
 import {shallow} from "zustand/shallow";
 import useProducts from "@/hooks/use-products";
+import {Label} from "@/components/ui/label";
 
 export function ProductsPagination() {
   const total = useProducts().data?.length ?? 0;
@@ -25,30 +26,34 @@ export function ProductsPagination() {
   }, [params])
 
   return (
-    <Pagination className="justify-end">
-      <PaginationContent className="mt-4">
-        {(total > 1 && params.pageNumber !== 1) && (
-          <PaginationItem>
-            <PaginationPrevious onClick={() => changePage(params.pageNumber! - 1)}/>
-          </PaginationItem>
-        )}
-        {Array.from({length: total / params.pageSize! + 1}, (_, i) => i + 1).map((page) => {
-          if (page < params.pageNumber! - 2 || page > params.pageNumber! + 2) {
-            return null
-          }
-          return (
-            <PaginationItem key={page}>
-              <PaginationLink isActive={page === params.pageNumber}
-                              onClick={() => changePage(page)}>{page}</PaginationLink>
+    <div className="flex flex-col">
+      <Label className="mt-3 mb-1">Pagination</Label>
+      <Pagination className="justify-end">
+        <PaginationContent>
+          {(total > 1 && params.pageNumber !== 1) && (
+            <PaginationItem>
+              <PaginationPrevious onClick={() => changePage(params.pageNumber! - 1)}/>
             </PaginationItem>
-          )
-        })}
-        {(total > 1 && params.pageNumber !== Math.floor(total / params.pageSize + 1)) && (
-          <PaginationItem>
-            <PaginationNext onClick={() => changePage(params.pageNumber! + 1)}/>
-          </PaginationItem>
-        )}
-      </PaginationContent>
-    </Pagination>
+          )}
+          {Array.from({length: total / params.pageSize! + 1}, (_, i) => i + 1).map((page) => {
+            if (page < params.pageNumber! - 2 || page > params.pageNumber! + 2) {
+              return null
+            }
+            return (
+              <PaginationItem key={page}>
+                <PaginationLink isActive={page === params.pageNumber}
+                                onClick={() => changePage(page)}>{page}</PaginationLink>
+              </PaginationItem>
+            )
+          })}
+          {(total > 1 && params.pageNumber !== Math.floor(total / params.pageSize! + 1)) && (
+            <PaginationItem>
+              <PaginationNext onClick={() => changePage(params.pageNumber! + 1)}/>
+            </PaginationItem>
+          )}
+        </PaginationContent>
+      </Pagination>
+
+    </div>
   )
 }
