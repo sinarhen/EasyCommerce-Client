ef07698536a8859a857b6dcb8c4e656d5decea3a
sinarhen <nicklchannel8@gmail.com>
2024-03-24 14:14:59 +0200

    categories-breadcrumbs.tsx on click

--- src/components/ui/skeletons/categories-breadcrumbs.tsx
+++ src/components/ui/skeletons/categories-breadcrumbs.tsx
@@ -4,13 +4,16 @@ import {ProductCategoryDto} from "@/types/product";
 
 export default function CategoriesBreadcrumbs({
   categories,
+  onClick
                                               }: {
-  categories: ProductCategoryDto[]
+  categories: ProductCategoryDto[];
+  onClick?: (category: ProductCategoryDto) => void;
 }) {
   return (
     <>
       {categories.map((category, index) => (
         <span key={category.id}
+              onClick={() => onClick && onClick(category)}
               className="text-gray-400 text-nowrap">{category.name}{index !== categories.length - 1 && ","}</span>
       ))}
     </>
